9d63e3acab8998f2aa2e92733ef39d982dfb92db
Rahi <rahishah1598@gmail.com>
2019-11-04 17:39:26 +0530

    solved errors

--- Housing/Housing.py
+++ Housing/Housing.py
@@ -12,23 +12,24 @@ HOUSING_URL=DOWNLOAD_ROOT+"datasets/housing/housing.tgz"
 
 
 def fetch_housing_data(housing_url=HOUSING_URL,housing_path=HOUSING_PATH):
+	
 	''' Fetches Data and extract it '''
 
-    	if not os.path.isdir(housing_path):
-        	os.makedirs(housing_path)
-    	tgz_path=os.path.join(housing_path,"housing.tgz")
-    	urllib.request.urlretrieve(housing_url,tgz_path)
-    	housing_tgz=tarfile.open(tgz_path)
-    	housing_tgz.extractall(path=housing_path)
-    	housing_tgz.close()
-    	print(tgz_path)
+	if not os.path.isdir(housing_path):
+		os.makedirs(housing_path)
+	tgz_path=os.path.join(housing_path,"housing.tgz")
+	urllib.request.urlretrieve(housing_url,tgz_path)
+	housing_tgz=tarfile.open(tgz_path)
+	housing_tgz.extractall(path=housing_path)
+	housing_tgz.close()
+	print(tgz_path)
 
 def load_housing_data(housing_path=HOUSING_PATH):
 	csv_path=os.path.join(housing_path,"housing.csv")
 	return pd.read_csv(csv_path)
 
 def main():
-	#fetch_housing_data()
+	fetch_housing_data()
 	housing=load_housing_data()
 	print("Head data is \n {}".format(housing.head()))
 	print("info data is \n {}".format(housing.info()))
